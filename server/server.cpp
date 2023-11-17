2b76dfae20bd85910ab42025e494c60589218f98
Dageus <101069446+Dageus@users.noreply.github.com>
2023-11-17 17:49:46 +0000

    Initial commit

---
+++ server/server.cpp
@@ -0,0 +1,24 @@
+
+
+int main(){
+
+
+    return 0;
+}
+
+
+
+
+/**
+ * 
+ * Estrutura de quando um cliente se liga ao servidor:
+ *  - O cliente envia uma mensagem ao servidor
+ *  - criar (uid)_pass.txt
+ *  - criar (uid)_login.txt
+ * 
+ * ESTES FICHEIROS SAO TEMPORARIOS E SO EXISTEM ENQUANTO O USER ESTIVER LOGGED IN
+ * 
+ * SEMPRE QUE O UTILIZADOR VOLTA A ENTRAR È CRIADO UM NOVO FICHEIRO PASS E LOGIN
+ * 
+ * 
+*/
