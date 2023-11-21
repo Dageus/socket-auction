78b8194f9c66d33d3c37cbc0dfa20b34caef4c77
Dageus <101069446+Dageus@users.noreply.github.com>
2023-11-21 12:27:28 +0000

    mudar validate_args

--- client/client.c
+++ client/client.c
@@ -15,7 +15,7 @@
 #define TEST_PORT 58011
 #define DEFAULT_PORT 58090
 
-#define SERVER_IP "localhost"
+#define LOCAL_SERVER_IP "localhost"
 
 
 int fd,errcode;
@@ -25,88 +25,61 @@ struct addrinfo hints, *res;
 struct sockaddr_in addr;
 char buffer[128];
 
-// initialize variables
-int fd, n;
-
-int ip, port;
 
 // initialize default values in case of incomplete command
 
-ip = SERVER_IP;
-port = DEFAULT_PORT;
+char *ip = LOCAL_SERVER_IP;
+int port = DEFAULT_PORT;
 
 int validate_args(int argc, char** argv) {
 
-    if (argc == 1) {
-        // run AS on localhost
-        // no port needed to be specified
+    int opt;
 
-        printf("Running AS on localhost\n");
-    } else if (argc == 3) {
-        // run AS on specified IP
-        // no port needed to be specified
-
-        if (strcmp(argv[1], "-n") != 0) {
-            // error
-            // incomplete command, missing -n
-            exit(1);
+    while ((opt = getopt(argc, argv, "n:p:")) != -1) {
+        switch (opt) {
+            case 'n':
+                char *ip = optarg;
+                printf("ip: %s\n", ip);
+                break;
+            case 'p':
+                port = atoi(optarg);
+                printf("port: %d\n", port);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n ip] [-p port]\n", argv[0]);
+                exit(EXIT_FAILURE);
         }
-
-        ip = atoi(argv[2]);
-
-        printf("Running AS on specified IP: %d\n", ip);
-    } else if (argc >= 5) {
-        // run AS on specified IP
-        // run AS on specified port
-
-        if (strcmp(argv[1], "-n") != 0) {
-            // error
-            // incomplete command, missing -n
-
-            printf("Error: missing -n argument\n");
-
-            exit(1);
-        } else if (strcmp(argv[3], "-p") != 0) {
-            // error
-            // incomplete command, missing -p
-
-            printf("Error: missing -p argument\n");
-
-            exit(1);
-        }
-
-        ip = atoi(argv[2]);
-        port = atoi(argv[4]);
-
-        printf("Running AS on specified IP: %d and specified port: %d\n", ip, port);
     }
+
+    if (argc == 1)
+        printf("ip: %s\n", ip);
 }
 
 void process_cmd(char* input){
     if (strcmp(input, "login") == 0) {
-        process_login();
+        // process_login();
     } else if (strcmp(input, "logout") == 0) {
-        process_logout();
+        // process_logout();
     } else if (strcmp(input, "unregister") == 0) {
-        process_unregister();
+        // process_unregister();
     } else if (strcmp(input, "exit") == 0) {
-        process_exit();
+        // process_exit();
     } else if (strcmp(input, "open") == 0) {
-        process_open();
+        // process_open();
     } else if (strcmp(input, "close") == 0) {
-        process_close();
+        // process_close();
     } else if (strcmp(input, "myauctions") == 0 || strcmp(input, "ma") == 0) {
-        process_auctions();
+        // process_auctions();
     } else if (strcmp(input, "mybids") == 0 || strcmp(input, "mb") == 0) {
-        process_bids();
+        // process_bids();
     } else if (strcmp(input, "list") == 0 || strcmp(input, "l") == 0) {
-        process_list();
+        // process_list();
     } else if (strcmp(input, "show_asset") == 0 || strcmp(input, "sa") == 0) {
-        process_asset();
+        // process_asset();
     } else if (strcmp(input, "bid") == 0 || strcmp(input, "b") == 0) {
-        process_bid();
+        // process_bid();
     } else if (strcmp(input, "show_record") == 0 || strcmp(input, "sr") == 0) {
-        process_record();
+        // process_record();
         
     } else {
         fprintf(stderr, "error: unkown_command\n");
@@ -132,7 +105,7 @@ int main(int argc, char** argv) {
     // main code
     while (TRUE) {
 
-        char* input[MAX_COMMAND_LEN];
+        char *input = (char *) malloc(sizeof(char) * MAX_COMMAND_LEN);
 
         // read from terminal
         scanf("%s", input);
