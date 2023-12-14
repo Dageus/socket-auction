0542cdc8a066ba7768e2a6d21f26550306f7ca95
Dageus <101069446+Dageus@users.noreply.github.com>
2023-12-14 18:47:57 +0000

    bid is done

--- server/TCP/bid/bid.h
+++ server/TCP/bid/bid.h
@@ -4,7 +4,7 @@
 #define BID_CMD         "RBD"
 #define BID_NOK_LEN     8
 #define BID_ERR_LEN     8
-#define BID_OK_LEN      7
+#define BID_ACC_LEN     8
 
 int process_bid(char *input, char **response);
 
